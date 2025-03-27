8b14019f1564250986aa0367e95dc807e81b62b0
Izanagi078 <kashyaparman15@gmail.com>
2025-03-27 23:19:57 +0530

    Did

--- Newbie.cpp
+++ Newbie.cpp
@@ -33,6 +33,35 @@ private:
             inOrderHelper(node->right);
         }
     }
+    void clearTree(Node *node) {
+        if (node == TNULL) return;
+        clearTree(node->left);
+        clearTree(node->right);
+        delete node;
+    }
+    void leftRotate(Node *x) {
+        Node *y = x->right;
+        x->right = y->left;
+        if (y->left != TNULL) y->left->parent = x;
+        y->parent = x->parent;
+        if (!x->parent) root = y;
+        else if (x == x->parent->left) x->parent->left = y;
+        else x->parent->right = y;
+        y->left = x;
+        x->parent = y;
+    }
+
+    void rightRotate(Node *x) {
+        Node *y = x->left;
+        x->left = y->right;
+        if (y->right != TNULL) y->right->parent = x;
+        y->parent = x->parent;
+        if (!x->parent) root = y;
+        else if (x == x->parent->right) x->parent->right = y;
+        else x->parent->left = y;
+        y->right = x;
+        x->parent = y;
+    }
 public:
     RedBlackTree() {
         TNULL = new Node;
